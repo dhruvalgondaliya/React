fa2b02293702daf43054c2d601347c4b351b8a0e
Dhruval Patel <f342dhruval@gmail.com>
2024-03-21 13:49:07 +0530

    first commit

---
+++ taskone/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import ContentOne from './modules/Counter project/ContentOne';
+import Footer from './modules/Counter project/Footer';
+import Header from './modules/Counter project/Header';
+import Sidebar from './modules/Counter project/SideBar';
+// import Content from './modules/Counter project/Content'
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <ContentOne/>
+
+      <Sidebar />
+      <Footer/>
+    </>
+
+  );
+}
+
+export default App;
---
+++ taskone/src/modules/Counter project/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Style.css'
+
+export default function Footer() {
+    return (
+        <>
+{/* 
+            <div className="footer">
+                <p>This is a simple footer</p>
+            </div> */}
+
+
+        </>
+    )
+}
