17bbac3226d920a39f5988aa7db01c429667020a
Dhruval Patel <f342dhruval@gmail.com>
2024-03-23 11:35:25 +0530

    second project upload

--- taskone/src/App.js
+++ taskone/src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
-import ContentOne from './modules/Counter project/ContentOne';
-import Footer from './modules/Counter project/Footer';
-import Header from './modules/Counter project/Header';
-import Sidebar from './modules/Counter project/SideBar';
-// import Content from './modules/Counter project/Content'
+import Arry from './modules/ConceptArry/Arry';
+// import ContentOne from './modules/Counter project/ContentOne';
+// import Footer from './modules/Counter project/Footer';
+// import Header from './modules/Counter project/Header';
+// import Sidebar from './modules/Counter project/SideBar';
 
 
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header />
       <ContentOne/>
-
       <Sidebar />
-      <Footer/>
+      <Footer/> */}
+      <Arry />
     </>
 
   );
