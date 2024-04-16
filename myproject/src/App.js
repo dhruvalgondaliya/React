d310417def6beeb731ab1f1ed05d8a9214deb0b1
Dhruval Patel <f342dhruval@gmail.com>
2024-04-16 10:52:10 +0530

    Routes folder add

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Home from './Routes/Home';
+import About from './Routes/About';
+import Contact from './Routes/Contact';
 // import Router from './component/Router';
-import Home from './component/Home';
-import About from './component/About';
-import Contact from './component/Contact';
+
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/Contact' element={<Contact />} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/Contact' element={<Contact/>} />
       </Routes>
 
     </>
---
+++ myproject/src/Routes/Contact.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
+
+
+export default function About() {
+
+    const navigate = useNavigate()
+
+    const gotoHome = () => {
+        navigate('/')
+    }
+
+    return (
+        <>
+            <div className='text-center'>
+                <h1>I am Contact Page</h1>
+                {/* first method */}
+                {/* <Link to="/Contact">Go To Contact Page</Link> */}
+                
+                {/* second method */}
+                <button onClick={gotoHome}>GO TO HOME PAGE</button>
+            </div>
+        </>
+    )
+}
