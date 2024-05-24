ffc0d8e99a1e584fb3165a470e61b46759dfc02c
Dhruval Patel <f342dhruval@gmail.com>
2024-05-24 19:23:52 +0530

    add firebase crud in Edit delete

--- crudfirebase/src/firebase.js
+++ crudfirebase/src/firebase.js
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDWSqZFP_3Wgkdq4bbuyDN1N025Ddlcg4k",
-  authDomain: "fir-crud-beb70.firebaseapp.com",
-  projectId: "fir-crud-beb70",
-  storageBucket: "fir-crud-beb70.appspot.com",
-  messagingSenderId: "596032863555",
-  appId: "1:596032863555:web:4e4b34610433be580303f6",
-  measurementId: "G-VSB5HGKSDD"
+    apiKey: "AIzaSyDWSqZFP_3Wgkdq4bbuyDN1N025Ddlcg4k",
+    authDomain: "fir-crud-beb70.firebaseapp.com",
+    projectId: "fir-crud-beb70",
+    storageBucket: "fir-crud-beb70.appspot.com",
+    messagingSenderId: "596032863555",
+    appId: "1:596032863555:web:4e4b34610433be580303f6",
+    measurementId: "G-VSB5HGKSDD"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const database = getFirestore(app);
+export default database;
